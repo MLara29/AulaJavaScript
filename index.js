0fd377152b7e80c0e3e7850fa03c2d3fca8ce04c
Miqueias Lara <miqueias.lara@gmail.com>
2024-01-25 18:40:03 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+var idade = prompt("Digite sua Idade");
+
+
+
+var ano = 2024;
+
+var anoNascimento;
+
+anoNascimento = (parseInt(idade) - ano)+1;
+
+alert("Você Nasceu em " + anoNascimento);
+
+console.log("Você Nasceu em " + anoNascimento);
