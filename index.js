aa1780c6ffef4ee1bf45c8c47c16d7c2c3a7e29d
Miqueias Lara <miqueias.lara@gmail.com>
2024-03-04 18:07:19 -0300

    melhorado codigo java

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+let flag = false;
 
 
 class carro{
@@ -16,38 +16,68 @@ class carro{
     
 
 
-
 const Carros = []
 
 Carros.push(new carro("Audi", "A1", 2017, "Preta"))
 
-Carros.push(new carro("GM", "CRUZE", 2019, "Branco"))
+Carros.push(new carro("Chevrolet", "CRUZE", 2019, "Branco"))
 
 Carros.push(new carro("Honda", "CIVIC", 2019, "Branco"))
 
-Carros.push(new carro("Honda", "FIT", 2020, "Vermelho"))
+Carros.push(new carro("Volkswagem", "Golf", 2020, "Vermelho"))
 
 Carros.push(new carro("Fiat", "Fiat 500", 2022, "Azul"))
 
 
-const Veiculo = prompt("Qual Veiculo você Procura ?" );
+const Pergunta = prompt("Digite o que você quer Fazer, Comprar ou Vender Seu Veiculo ?")
 
-for (const item of Carros )
-{
+if(Pergunta === "Comprar" ){
+
+
+  const Veiculo = prompt("Qual Veiculo você Procura ?" );
+
+
+  for(let i = 0; i <= Carros.length; i++){
+
+    if(Carros[i].marca === Veiculo){
+      alert("Seu Carro Foi Encontrado!!") 
+      flag = true;
+      console.log(Carros[i].marca);
+    } 
     
-
-    if(item.marca === Veiculo || item.modelo === Veiculo  )
-    {
-        alert("Seua Carro foi encontardo !!")
-        console.log(item)
-        
+    if((i == (Carros.length-1))  &&  Carros[i].marca !== Veiculo && flag == false){
+      alert("Seu Carro não Foi Encontrado!!") 
+      
+      console.log(i);
     }
-    if(item.marca !== Veiculo || item.modelo !== Veiculo  )
-    {
-        alert("Seua Carro nâo foi encontardo !!")
-    }
+ 
+  }
     
 }
 
+else if (Pergunta === "Vender"){
+
+  let VeiculoVendaMarca = prompt("Digite  a Marca do Seu Veiculo !!")
+
+  let VeiculoVendaModelo = prompt("Digite  o Modelo do Seu Veiculo !!")
+
+  let VeiculoVendaAno = prompt("Digite  o Ano do Seu Veiculo !!")
+
+  let VeiculoVendaCor = prompt("Digite  a Cor do Seu Veiculo !!")
 
 
+  
+  
+  Carros.push(new carro(VeiculoVendaMarca, VeiculoVendaModelo, VeiculoVendaAno, VeiculoVendaCor))
+
+  alert("Obrigado seu Carro Foi Adicionado ao Banco de Dados e Quando aparecer alguma proposta você sera Avisado !!")
+  console.log(Carros);
+}
+  
+
+   
+
+  
+
+
+  
